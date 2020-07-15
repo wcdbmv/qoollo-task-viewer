22738f5e739ab122cb896161473c5973f02c74cd
Azamat <azamagsanginov01@gmail.com>
2020-07-15 20:14:41 +0300

    Added ApiServices.

---
+++ QoolloTaskViewer/QoolloTaskViewer.ApiServices/Dtos/IssueDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QoolloTaskViewer.ApiServices
+{
+    public class Label
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Milestone
+    {
+        public string due_on { get; set; }
+    }
+
+    public class IssueDto
+    {
+        public string title { get; set; }
+        public string state { get; set; }
+
+        public string body { get; set; }
+
+        public List<Label> labels { get; set; }
+        public Milestone milestone { get; set; }
+
+        public string html_url { get; set; }
+    }
+}
---
+++ QoolloTaskViewer/QoolloTaskViewer.ApiServices/IApiService.cs
@@ -0,0 +1,14 @@
+﻿using QoolloTaskViewer.ApiServices;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QoolloTaskViewer.ApiServices
+{
+    public interface IApiService
+    {
+        Task<List<IssueDto>> GeatAllMyIssues();
+    }
+}
