66fa040690b7f96a94bd5c0d89d546a8dc9241be
Ahmed Kerimov <kerimov.dev@yandex.ru>
2020-07-17 15:41:11 +0300

    Add-Migration Initial

---
+++ QoolloTaskViewer/QoolloTaskViewer/Migrations/20200717122941_Initial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QoolloTaskViewer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Domains",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Domain = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Domains", x => x.Id);
+                    table.UniqueConstraint("AK_Domains_Domain", x => x.Domain);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Username = table.Column<string>(maxLength: 255, nullable: false),
+                    PasswordHash = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.UniqueConstraint("AK_Users_Username", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    DomainId = table.Column<Guid>(nullable: false),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                    table.UniqueConstraint("AK_Services_Name", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_Services_Domains_DomainId",
+                        column: x => x.DomainId,
+                        principalTable: "Domains",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    ServiceId = table.Column<Guid>(nullable: false),
+                    InServiceUsername = table.Column<string>(maxLength: 255, nullable: false),
+                    Token = table.Column<string>(nullable: false),
+                    Enabled = table.Column<bool>(nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Domains",
+                columns: new[] { "Id", "Domain" },
+                values: new object[,]
+                {
+                    { new Guid("fd31fdcd-0ebf-48aa-9362-bbfd910f5030"), "github.com" },
+                    { new Guid("a78ff080-9fa9-4c26-8a87-23aa04a52d55"), "gitlab.com" },
+                    { new Guid("eb03b789-7b52-4e2d-9dc0-f51d048dd81a"), "jira.atlassian.com" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_DomainId",
+                table: "Services",
+                column: "DomainId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_ServiceId",
+                table: "Tokens",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_UserId",
+                table: "Tokens",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tokens");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Domains");
+        }
+    }
+}
