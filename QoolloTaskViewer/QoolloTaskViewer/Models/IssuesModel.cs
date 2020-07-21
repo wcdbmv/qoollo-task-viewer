1bf95a6d80a0fd56deb23f927f70b2bfeeb61ba3
MNovikov <novikov.mikael@gmail.com>
2020-07-21 15:19:28 +0300

    Board's view (with test data) was completed

---
+++ QoolloTaskViewer/QoolloTaskViewer/Models/IssuesModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QoolloTaskViewer.ApiServices.Dtos;
+
+namespace QoolloTaskViewer.Models
+{
+    public class Issues
+    {
+        public List<IssueDto> UnrecognizedIssues { get; set; }
+        public List<IssueDto> ToDoIssues { get; set; }
+        public List<IssueDto> DoingIssues { get; set; }
+        public List<IssueDto> ReviewIssues { get; set; }
+
+        public Issues()
+        {
+            UnrecognizedIssues = new List<IssueDto> { };
+            ToDoIssues = new List<IssueDto> { };
+            DoingIssues = new List<IssueDto> { };
+            ReviewIssues = new List<IssueDto> { };
+        }
+    }
+}
